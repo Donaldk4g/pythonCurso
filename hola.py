cd183b70bfd59d8f3a6fa50eba8811506cb9d6c7
Donaldk4g <dogoca17@gmail.com>
2024-02-17 22:49:56 +0100

    codigo de trabajo

--- hola.py
+++ hola.py
@@ -1,1 +1,20 @@
-print('hola donald')
+import libreria as funci
+import matplotlib.pyplot as plt
+
+ruta="C:/Users/dogoc/Desktop/cursos/python_curso1/data.csv"
+
+datos=funci.DataLoad(ruta)
+selecion=input('ingrese el pais a graficar: ')
+dato=funci.selePais(selecion,datos)
+
+x = dato.keys()
+y = dato.values()
+
+fig, ax = plt.subplots()
+ax.bar(x, y, width=1, edgecolor="white", linewidth=0.7)
+plt.savefig(f"{selecion}")
+plt.close()
+
+
+
+
---
+++ libreria.py
@@ -0,0 +1,59 @@
+
+import csv
+
+def DataLoad(ruta):    
+    with open(ruta, 'r') as csvfile:
+
+        lineas= csv.reader(csvfile, delimiter=',')
+        header = next(lineas)
+        data=[]
+        for row in lineas:
+            iterable=zip(header, row)
+            dict = {key: value for key, value in iterable} 
+            data.append(dict)         
+    return data
+
+def selePais(NombrePais, datos):
+    for i in datos:
+        if i['Country/Territory']==NombrePais.capitalize():
+
+           dic= { 
+            '2022': int(i['2022 Population']),
+            '2020': int(i['2020 Population']),
+            '2015': int(i['2015 Population']),
+            '2010': int(i['2010 Population']),
+            '2000': int(i['2000 Population']),
+            '1990': int(i['1990 Population']),
+            '1980': int(i['1980 Population']),
+            '1970': int(i['1970 Population']),
+            }
+    return dic
+
+def DataLoad2(ruta):    
+    with open(ruta, 'r') as csvfile:
+
+        lineas= csv.reader(csvfile, delimiter=',')
+        dato = next(lineas)
+        pais = dato.index('Country/Territory')
+        densidad=dato.index('World Population Percentage')
+        paises=[]
+        densidades=[]
+        for i in lineas:
+            densidades.append(i[densidad])
+            paises.append(i[pais])      
+        
+    return paises, densidades
+
+if __name__ == '__main__':
+
+    
+        ruta="./data.csv"
+
+        datos=DataLoad(ruta)
+
+        NombrePais=input('ingrese el pais a graficar: ')
+
+        print(selePais(NombrePais,datos))
+
+
+
